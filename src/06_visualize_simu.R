8167a7e083bf010136a048aa264cee54e8bd03ee
anastasia-lucas <anastasia.lucas.bioinfo@gmail.com>
2023-05-04 14:56:06 -0400

    feat: all metrics

--- src/06_visualize_simu.R
+++ src/06_visualize_simu.R
@@ -23,7 +23,10 @@ set.seed(42)
 data.path <- file.path("snapshots", "summary-disease-all.txt")
 
 # outputs
+out.path <- file.path("snapshots", "simulation_results")
 
+# variables 
+pal <- c("#0072B2", "#009E73", "#E69F00", "#D55E00")
 
 ####################################################
 #                     functions                    #  
@@ -42,14 +45,30 @@ data$cc <- sub("_.*", "", sub(".*param_", "", data$dataset))
 data$or <- sub("\\.param.*", "", sub(".*or_", "", data$dataset))
 data$prev <- sub("\\.raw", "", sub(".*_", "", data$dataset))
 
-ggplot(data = data[data$metric %in% c("cor.freq.case", "cor.freq.ctrl") &
-                     data$or != "1-10.0",], 
-       aes(x = edges.med,
-           y = value,
-           shape = cc,
-           color = prev)) +
-  geom_point() +
-  facet_grid(maf ~ or) +
-  theme_bw()
+for(i in unique(data$metric)){
+  
+  p <- ggplot(data = data[data$metric == i & data$or != "1-10.0",], 
+              aes(x = edges.med,
+                  y = value,
+                  shape = cc,
+                  color = prev)) +
+    geom_point(size = 2) +
+    facet_grid(maf ~ or) +
+    theme_bw() +
+    theme(panel.grid.major.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          strip.background = element_rect(fill = "white")) +
+    scale_color_manual(values = pal) +
+    ggtitle(i)
+  
+  print(paste("Saving...", i))
+  
+  ggsave(p, filename = paste0(out.path, "-", i, ".pdf"), height = 5.5, width = 9)
+  
+}
+
+
+
 
 
